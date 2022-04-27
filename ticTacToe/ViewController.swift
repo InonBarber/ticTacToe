d3cc2858d7ea36a6db27daab9c094715050f40b2
inon barber <inonbarber110@gmail.com>
2022-04-27 21:37:28 +0300

    Initial Commit

---
+++ ticTacToe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ticTacToe
+//
+//  Created by Inon Barber on 27/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
